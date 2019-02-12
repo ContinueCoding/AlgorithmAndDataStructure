7bc7807b4292bc0787cc951ae14777b437f092d3
huoxiaoyi <huoxiaoyi@didapinche.com>
2019-02-12 14:00:09 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * 算法与数据结构
+ *
+ *  P.S.
+ *   https://visualgo.net/en 各个算法的动画效果！
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("algorithm and data structure...");
+    }
+}
