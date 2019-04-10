48c088e38a22fc5fa712db8e7f416b1077471b4d
huoxiaoyi <huoxiaoyi@didapinche.com>
2019-04-10 18:07:13 +0800

    数据结构与算法之美 - 数组：实现泛型数组类 - 获取长度、容量、添加、删除元素等。

--- src/beauty_of_data_structure_and_algorithm/ArrayDemo.java
+++ src/beauty_of_data_structure_and_algorithm/ArrayDemo.java
@@ -52,6 +52,160 @@ public class ArrayDemo {
         return true;
     }
 
+    //TODO 自定义泛型Array类
+    static class GenericArray<T> {
+        private T[] data;
+        private int size;
+
+        public GenericArray() {
+            this(10);
+        }
+
+        public GenericArray(int capacity) {
+            data = (T[]) new Object[capacity];
+            size = 0;
+        }
+
+        //获取容量
+        public int getCapacity() {
+            return data.length;
+        }
+
+        // 获取当前元素个数
+        public int count() {
+            return size;
+        }
+
+        // 判断数组是否为空
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        // 修改 index 位置的元素
+        public void setIndex(int index, T e) {
+            checkIndex(index);
+            data[index] = e;
+        }
+
+        // 获取对应 index 位置的元素
+        public T getIndex(int index) {
+            checkIndex(index);
+            return data[index];
+        }
+
+        // 查看数组是否包含元素e
+        public boolean contains(T e) {
+            for (int i = 0; i < size; i++) {
+                if(data[i].equals(e)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        // 获取对应元素的下标, 未找到，返回 -1
+        public int findIndexOf(T e) {
+            for (int i = 0; i < size; i++) {
+                if(data[i].equals(e)) {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        // 在 index 位置，插入元素e(其他元素往后移), 时间复杂度 O(m+n)
+        public void add(int index, T e) {
+            checkIndex(index);
+
+            //扩容：2倍
+            if(size == data.length) {
+                resize(size * 2);
+            }
+
+            //搬移数据
+            for (int i = size - 1; i >= index; i--) {
+                data[i + 1] = data[i];
+            }
+
+            //插入数据到指定位置
+            data[index] = e;
+            size ++;
+        }
+
+        // 向数组头插入元素
+        public void addFirst(T e) {
+            add(0, e);
+        }
+
+        // 向数组尾插入元素
+        public void addLast(T e) {
+            add(size, e);
+        }
+
+        // 删除 index 位置的元素，并返回
+        public T remove(int index) {
+            checkIndexForRemove(index);
+
+            T result = data[index];
+            for (int i = index; i < size - 1; i++) {
+                data[i] = data[i + 1];
+            }
+            data[size - 1] = null;
+            size --;
+
+            /*
+            //TODO 缩容(条件)
+            if (size == data.length / 4 && data.length / 2 != 0) {
+                resize(data.length / 2);
+            }
+            */
+
+            return result;
+        }
+
+        public T removeFirst() {
+            return remove(0);
+        }
+
+        public T removeLast() {
+            return remove(size - 1);
+        }
+
+        public void removeElement(T e) {
+            int index = findIndexOf(e);
+            if(index != -1) {
+                remove(index);
+            }
+        }
+
+        //-----------------private方法------------------
+
+        //扩容方法，时间复杂度 O(n)
+        private void resize(int capacity) {
+            T[] newData = (T[]) new Object[capacity];
+
+            for (int i = 0; i < size; i++) {
+                newData[i] = data[i];
+            }
+
+            data = newData;
+        }
+
+        private void checkIndex(int index) {
+            if(index < 0 || index > size) {
+                throw new IllegalArgumentException("Invalid index for add/set: " + index);
+            }
+        }
+
+        private void checkIndexForRemove(int index) {
+            if(index < 0 || index >= size) {
+                throw new IllegalArgumentException("Invalid index for remove: " + index);
+            }
+        }
+    }
+
     /**
      * insert()改进：数据搬移耗费时间，对于不需要有序的数组，可以直接把
      *              index原有数据搬到数组末尾，然后把新值插入到index位置！
