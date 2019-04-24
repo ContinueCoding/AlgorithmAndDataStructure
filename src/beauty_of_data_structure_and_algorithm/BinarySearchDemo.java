c22e27e8633ecbfc94382e17087f8e5a34ac9449
huoxiaoyi <huoxiaoyi@didapinche.com>
2019-04-24 18:24:39 +0800

    数据结构与算法之美 - 二分查找：二分思想求平方根。

--- src/beauty_of_data_structure_and_algorithm/BinarySearchDemo.java
+++ src/beauty_of_data_structure_and_algorithm/BinarySearchDemo.java
@@ -13,6 +13,8 @@ package beauty_of_data_structure_and_algorithm;
  */
 public class BinarySearchDemo {
 
+    public static final double EPSILON = 0.000001;
+
     public static void main(String[] args) {
 
         int[] array = {3, 12, 16, 28, 33, 45, 58, 60, 72, 81, 90, 99};
@@ -21,6 +23,8 @@ public class BinarySearchDemo {
 
         System.out.println("递归：90在array的index = " + binarySearch2(array, 90));
 
+        System.out.println("二分求平方根：24.6的平方根 = " + getSqrt(24.6));
+        System.out.println("Math.sqrt()求平方根：24.6的平方根 = " + Math.sqrt(24.6));
     }
 
     /**
@@ -74,6 +78,36 @@ public class BinarySearchDemo {
         }
 
         return -1;
+    }
+
+    /**
+     * 2 - 使用二分法求解一个数的平方根（正数），要求精确到小数点后6位。
+     *
+     * @param value value(>0)
+     * @return value的平方根（正数）
+     */
+    private static double getSqrt(double value) {
+        double low = 0;
+        double high = Math.max(value, 1);
+        double result = low + (high - low) / 2;
+
+        int count = 1;
+
+        while(Math.abs(result * result - value) > EPSILON & count < 100) {
+
+            if(result * result < value) {
+                low = result;
+            } else {
+                high = result;
+            }
+
+            result = low + (high - low) / 2;
+
+            count++;
+        }
+
+        System.out.println("getSqrt() - count = " + count);
+        return result;
     }
 
 }
